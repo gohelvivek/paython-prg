caed5b1580998df824c02c43ac68ecfb7db958ef
LOGAN3242 <omsarvaiya3242@gmail.com>
2022-02-17 12:19:01 +0530

    unit3 pdf list prg

---
+++ list prg unit 3/13.towerofhonoi prg.py
@@ -0,0 +1,9 @@
+def towerOfHanoi(N , source, destination, auxiliary):
+    if N==1:
+        print("Move disk 1 from source",source,"todestination",destination)
+        return
+    towerOfHanoi(N-1, source, auxiliary, destination)
+    print("Move disk",N,"from source",source,"todestination",destination)
+    towerOfHanoi(N-1, auxiliary, destination, source)
+N = 3
+towerOfHanoi(N,'A','B','C')
