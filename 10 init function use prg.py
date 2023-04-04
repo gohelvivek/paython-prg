64cf69062a018794e926aacf8e62527e7437688c
Gohel vivek s <91551556+gohelvivek@users.noreply.github.com>
2023-04-04 22:53:30 +0530

    Update 10 init function use prg.py

--- 10 init function use prg.py
+++ 10 init function use prg.py
@@ -2,7 +2,9 @@ class Person:
     def __init__(self,name,age):
         self.name=name
         self.age=age
+        self.surname=surname
 p1=Person("John",36)
 print(p1.name)
 print(p1.age)
+print(p1.surname)
     
